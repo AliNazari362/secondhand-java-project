576daa687b515703e4a6d8cc730575449ffd33c6
student62 <mohammad.kherad@aut.ac.ir>
2026-07-16 18:28:48 +0330

    AlertUtill is added

--- frontend/src/main/java/utils/AlertUtil.java
+++ frontend/src/main/java/utils/AlertUtil.java
@@ -1,4 +1,30 @@
 package utils;
 
+import javafx.scene.control.Alert;
+
 public class AlertUtil {
+
+    public static void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("خطا");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static void showSuccess(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("موفق");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    public static void showWarning(String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("توجه");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
